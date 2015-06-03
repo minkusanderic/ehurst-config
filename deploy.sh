5101d75a411477329e8b8c1b55e6514957b8c185
Eric Hurst <minkusanderic@gmail.com>
2015-06-03 10:23:26 -0600

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 sudo apt-get install zsh
 sudo apt-get install vim
 
-file="zshrc"
+files="zshrc vimrc bashrc"
 
 mkdir -p ~/.old_configs
 
@@ -14,5 +14,5 @@ mkdir -p ~/.old_configs
 for file in $files; do
 	echo "Seting up .$file"
 	mv ~/.$file ~/.old_configs/$file
-	ln -s ./$file ~/.$file
+	ln -s `readlink -f .$file` `readlink -f ~/.$file`
 done
